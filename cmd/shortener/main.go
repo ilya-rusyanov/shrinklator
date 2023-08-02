2bd1e045131828761baa01592c5ca589faf36491
Ilya Rusyanov <rusyanov@monitorsoft.ru>
2023-08-02 16:56:33 +0300

    read cmdline configuration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/ilya-rusyanov/shrinklator/internal/server"
+import (
+	"github.com/ilya-rusyanov/shrinklator/internal/config"
+	"github.com/ilya-rusyanov/shrinklator/internal/server"
+)
 
 func main() {
+	config.Init()
 	server.Run()
 }
---
+++ internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "flag"
+
+// app configuration
+var Values struct {
+	ListenAddr string
+	BasePath   string
+}
+
+func Init() {
+	flag.StringVar(&Values.ListenAddr, "a", ":8080", "address and port to listen on")
+	flag.StringVar(&Values.BasePath, "b", "http://localhost:8000", "base path")
+	flag.Parse()
+}
