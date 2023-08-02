efe519c3ce46e00584b371701e9861b3ea44b40d
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-08-02 19:14:37 +0300

    fix default basepath port

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,6 @@ var Values struct {
 
 func Init() {
 	flag.StringVar(&Values.ListenAddr, "a", ":8080", "address and port to listen on")
-	flag.StringVar(&Values.BasePath, "b", "http://localhost:8000", "base path")
+	flag.StringVar(&Values.BasePath, "b", "http://localhost:8080", "base path")
 	flag.Parse()
 }
