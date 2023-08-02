1d3687b5c21011d8b2f2c5da545422f637f4d0b6
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-08-02 20:07:10 +0300

    using environment variables for cfg

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 // app configuration
 var Values struct {
@@ -12,4 +15,12 @@ func Init() {
 	flag.StringVar(&Values.ListenAddr, "a", ":8080", "address and port to listen on")
 	flag.StringVar(&Values.BasePath, "b", "http://localhost:8080", "base path")
 	flag.Parse()
+
+	if val, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+		Values.ListenAddr = val
+	}
+
+	if val, ok := os.LookupEnv("BASE_URL"); ok {
+		Values.BasePath = val
+	}
 }
