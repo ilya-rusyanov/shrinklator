01cc700942b3190a302c75782034479233c754c0
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-08-25 18:41:17 +0300

    log in memory storage access

--- internal/storage/in_memory.go
+++ internal/storage/in_memory.go
@@ -3,6 +3,9 @@ package storage
 import (
 	"errors"
 	"sync"
+
+	"github.com/ilya-rusyanov/shrinklator/internal/logger"
+	"go.uber.org/zap"
 )
 
 var errNotFound = errors.New("not found")
@@ -22,6 +25,9 @@ func (s *inMemory) Put(id, value string) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
+	logger.Log.Info("store", zap.String("id", id),
+		zap.String("value", value))
+
 	s.data[id] = value
 }
 
@@ -32,8 +38,12 @@ func (s *inMemory) ByID(id string) (string, error) {
 	value, ok := s.data[id]
 
 	if !ok {
+		logger.Log.Info("cannot find record", zap.String("id", id))
 		return "", errNotFound
 	}
+
+	logger.Log.Info("successuflly found record", zap.String("id", id),
+		zap.String("value", value))
 
 	return value, nil
 }
