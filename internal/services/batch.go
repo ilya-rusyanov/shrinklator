f279bc4a9ab298803f4ec5ab6b41208d47dcb58d
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-09-07 20:58:27 +0300

    batch shorten service impl

--- internal/services/batch.go
+++ internal/services/batch.go
@@ -25,5 +25,21 @@ func NewBatch(storage BatchStorage, algorithm Algo) *Batch {
 
 func (b *Batch) BatchShorten(ctx context.Context,
 	input []entities.BatchRequest) ([]entities.BatchResponse, error) {
-	return nil, fmt.Errorf("TODO")
+	toStore := make([]entities.ShortLongPair, len(input))
+	result := make([]entities.BatchResponse, len(input))
+
+	for i, val := range input {
+		toStore[i].Short = b.algo(val.LongURL)
+		toStore[i].Long = val.LongURL
+		result[i].ID = val.ID
+		result[i].ShortURL = toStore[i].Short
+	}
+
+	err := b.storage.PutBatch(ctx, toStore)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to store batch: %w", err)
+	}
+
+	return result, nil
 }
