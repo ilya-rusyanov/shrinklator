091f3bb307e94a78d3f9c73c53f904e25f5fd88b
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-09-10 19:06:58 +0300

    migration: long is unique

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -103,7 +103,7 @@ func (p *Postgres) ByID(ctx context.Context, id string) (string, error) {
 
 func migrate(ctx context.Context, log *logger.Log, db *sql.DB) error {
 	_, err := db.ExecContext(ctx,
-		`CREATE TABLE IF NOT EXISTS shorts (short text, long text,
+		`CREATE TABLE IF NOT EXISTS shorts (short text, long text UNIQUE,
 PRIMARY KEY (short)
 )`)
 	if err != nil {
